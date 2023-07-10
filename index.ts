53cf61a2f70001d72d8b66880c6f1e8d8574b477
Docenko Pasha <pashadocenko@gmail.com>
2023-07-10 13:01:56 +0300

    Express and MySQL work.

--- index.ts
+++ index.ts
@@ -1,13 +1,30 @@
-import express from 'express';
-import { Request, Response } from 'express';
+import express , {Express, Request, Response} from 'express';
+import mysql, {Connection} from 'mysql2';
 
-const app = express();
-const port = 3000;
+const app:Express = express();
+const port: number = 3000;
+
+interface configData {
+    host : string,
+    user : string,
+    password :string,
+    database : string
+}
+
+const configConnect : configData ={
+        host: 'localhost',
+        user: 'root',
+        password: 'Docenko4493!',
+        database: 'userdb1'
+}
+
+const connection : Connection  = mysql.createConnection(configConnect);
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello TypeScript and Node.js!');
+    res.send('Hello Pasha ,TypeScript and Node.js!');
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
+    await connection.connect();
     console.log(`Server running at http://localhost:${port}`);
 });
