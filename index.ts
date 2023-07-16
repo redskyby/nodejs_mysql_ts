217bac477cb76c267490e63b99fe7ceefb2cceea
Docenko Pasha <pashadocenko@gmail.com>
2023-07-16 21:42:20 +0300

    project is done. (CRUD)

--- index.ts
+++ index.ts
@@ -1,18 +1,17 @@
-import express , {Express} from 'express';
+import express, {Express} from 'express';
 import {config} from 'dotenv';
 import userRoutes from "./routes/user.routes";
 import connection from "./db";
 import postRoutes from "./routes/post.routes";
 
 config();
-const app:Express = express();
+const app: Express = express();
 const port: number = parseInt(process.env.PORT!, 10);
 
 
-
 app.use(express.json());
-app.use('/api' , userRoutes);
-app.use('/api' , postRoutes);
+app.use('/api', userRoutes);
+app.use('/api', postRoutes);
 
 app.listen(port, async () => {
     await connection.connect();
